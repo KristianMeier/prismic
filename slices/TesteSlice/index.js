80cb7a3ff47584b57c02f8cccfc287c49274f7fe
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-08-27 11:38:14 +0200

    Commit before refactor

--- slices/TesteSlice/index.js
+++ slices/TesteSlice/index.js
@@ -1,31 +1,14 @@
 import React from 'react'
 import { PrismicRichText } from '@prismicio/react'
 
-const TesteSlice = ({ slice }) => (
-  <section>
-    <span className="title">
-      {
-        slice.primary.title ?
-        <PrismicRichText field={slice.primary.title}/>
-        : <h2>Template slice, update me!</h2>
-      }
-    </span>
-    {
-      slice.primary.description ?
-      <PrismicRichText field={slice.primary.description}/>
-      : <p>start by editing this slice from inside Slice Machine!</p>
-    }
-    <style jsx>{`
-        section {
-          max-width: 600px;
-          margin: 4em auto;
-          text-align: center;
-        }
-        .title {
-          color: #8592e0;
-        }
-    `}</style>
-  </section>
-)
+const TesteSlice = ({ slice }) => {
+  return (
+    <section>
+      <PrismicRichText field={slice.primary.MinTitel} />
+      <PrismicRichText field={slice.primary.MinDescription} />
+      {console.log(slice)}
+    </section>
+  )
+}
 
 export default TesteSlice
