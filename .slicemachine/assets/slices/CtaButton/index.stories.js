85b6c36dad1ca9d4312f1a0432e72d384ec14339
KristianMeier <85857919+KristianMeier@users.noreply.github.com>
2022-08-27 09:36:57 +0200

    GUide https://prismic.io/docs/technologies/model-content-nextjs

---
+++ .slicemachine/assets/slices/CtaButton/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/CtaButton';
+
+export default {
+  title: 'slices/CtaButton'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"cta_button","items":[],"primary":{"title":[{"type":"heading1","text":"Synthesize 24/7 models","spans":[]}],"description":[{"type":"paragraph","text":"Occaecat cupidatat est exercitation ipsum sit sit ipsum adipisicing fugiat voluptate.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
